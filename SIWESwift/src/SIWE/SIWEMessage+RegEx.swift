7c6d861627284f314ccc41076c12d6c5b6fe6073
bigvo <vladcyoung@gmail.com>
2022-11-28 17:18:39 +0100

    Parser update

--- SIWESwift/src/SIWE/SIWEMessage+RegEx.swift
+++ SIWESwift/src/SIWE/SIWEMessage+RegEx.swift
@@ -18,25 +18,25 @@ extension SIWEMessage {
     private enum RegEx {
 
         static let domain = "(?<\(CodingKeys.domain.rawValue)>([^?#]*)) wants you to sign in with your Ethereum account:"
-        static let address = "\n(?<\(CodingKeys.address.rawValue)>0x[a-zA-Z0-9]{40})\n\n"
-        static let statement = "((?<\(CodingKeys.statement.rawValue)>[^\n]+)\n)?"
+        static let address = "\\n(?<\(CodingKeys.address.rawValue)>0x[a-zA-Z0-9]{40})\\n\\n"
+        static let statement = "((?<\(CodingKeys.statement.rawValue)>[^\\n]+)\\n)?"
         static let uri = "(([^:?#]+):)?(([^?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))"
-        static let uriLine = "\nURI: (?<\(CodingKeys.uri.rawValue)>\(uri)?)"
-        static let version = "\nVersion: (?<\(CodingKeys.version.rawValue)>1)"
-        static let chainId = "\nChain ID: (?<\(CodingKeys.chainId.rawValue)>[0-9]+)"
-        static let nonce = "\nNonce: (?<\(CodingKeys.nonce.rawValue)>[a-zA-Z0-9]{8,})"
+        static let uriLine = "\\nURI: (?<\(CodingKeys.uri.rawValue)>\(uri)?)"
+        static let version = "\\nVersion: (?<\(CodingKeys.version.rawValue)>1)"
+        static let chainId = "\\nChain ID: (?<\(CodingKeys.chainId.rawValue)>[0-9]+)"
+        static let nonce = "\\nNonce: (?<\(CodingKeys.nonce.rawValue)>[a-zA-Z0-9]{8,})"
         static let dateTime = "([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\\.[0-9]+)?(([Zz])|([\\+|\\-]([01][0-9]|2[0-3]):[0-5][0-9]))"
-        static let issuedAt = "\nIssued At: (?<\(CodingKeys.issuedAt.rawValue)>\(dateTime))"
+        static let issuedAt = "\\nIssued At: (?<\(CodingKeys.issuedAt.rawValue)>\(dateTime))"
         static let expirationTime = "(\nExpiration Time: (?<\(CodingKeys.expirationTime.rawValue)>\(dateTime)))?"
-        static let notBefore = "(\nNot Before: (?<\(CodingKeys.notBefore.rawValue)>\(dateTime)))?"
-        static let requestId = "(\nRequest ID: (?<\(CodingKeys.requestId.rawValue)>[-._~!$&'()*+,;=:@%a-zA-Z0-9]*))?"
-        static let resources = "(\nResources:(?<\(CodingKeys.resources.rawValue)>(\n- \(uri)?)+))?"
+        static let notBefore = "(\\nNot Before: (?<\(CodingKeys.notBefore.rawValue)>\(dateTime)))?"
+        static let requestId = "(\\nRequest ID: (?<\(CodingKeys.requestId.rawValue)>[-._~!$&'()*+,;=:@%a-zA-Z0-9]*))?"
+        static let resources = "(\\nResources:(?<\(CodingKeys.resources.rawValue)>(\\n- \(uri)?)+))?"
         static let message = "^\(domain)\(address)\(statement)\(uriLine)\(version)\(chainId)\(nonce)\(issuedAt)\(expirationTime)\(notBefore)\(requestId)\(resources)$"
     }
 
     init(fromStringUsingRegEx string: String) throws {
         guard let regex = try? NSRegularExpression(pattern: RegEx.message, options: []) else {
-            assertionFailure("Regular expression is invalid")
+            print(assertionFailure("Regular expression is invalid"))
             throw RegExError.noMatches
         }
         let range = NSRange(string.startIndex ..< string.endIndex, in: string)
